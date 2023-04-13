94d542c168462376fd1ec6789082dd9889096af2
c3kimball <c3kimball@student.bridgew.edu>
2023-04-13 09:41:05 -0400

    Initial commit

---
+++ src/ClientThread.java
@@ -0,0 +1,5 @@
+// Craig Kimball 4/13/23
+// COMP430 Project 2
+
+public class ClientThread extends Thread{
+}
---
+++ src/PhoneServer.java
@@ -0,0 +1,10 @@
+// Craig Kimball 4/13/23
+// COMP430 Project 2
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+
+public class PhoneServer {
+}
