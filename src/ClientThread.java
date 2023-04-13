660e6a197280a141c3d335b11b35860bfb0265bc
c3kimball <c3kimball@student.bridgew.edu>
2023-04-13 11:17:25 -0400

    Attempting to solve the issue of listener.accept() doing nothing.

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -1,5 +1,32 @@
 // Craig Kimball 4/13/23
 // COMP430 Project 2
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.*;
+
 public class ClientThread extends Thread{
+    Socket socket;
+
+    public ClientThread (Socket socket){
+        this.socket = socket;
+    }
+
+    public void run(){
+        handleConnection(socket);
+        System.out.println("Made it to run");
+    }
+
+    public void handleConnection(Socket socket){
+        try{
+            PrintStream stream = new PrintStream(socket.getOutputStream());
+            stream.println("Made it to handleConnection");
+            stream.close();
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
 }
--- src/PhoneServer.java
+++ src/PhoneServer.java
@@ -7,4 +7,22 @@ import java.net.*;
 
 
 public class PhoneServer {
+    // The port number on which the server will be listening
+    private static int port = 2014;
+    // The server socket.
+    private static ServerSocket listener = null;
+    // The client socket.
+    private static Socket clientSocket = null;
+
+    public static void main(String[] args) throws Exception{
+        ServerSocket server = new ServerSocket(port);
+        System.out.println("Server started.");
+        System.out.println("Waiting for clients...");
+
+        Socket clientSocket = server.accept();
+        System.out.println("Connected a client");
+        ClientThread client = new ClientThread(clientSocket);
+        client.start();
+
+    }
 }
